e79da932f9044373bddefcddf3b3f81c0418b2a8
vincent tordeux <vincent.tordeux@gmail.com>
2022-06-09 12:07:28 +0200

    panel admin fonctionnel

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -1,46 +1,49 @@
 <?php
 
 namespace App\Controller\Admin;
-
+use App\Entity\Pizza;
+use App\Entity\Menu;
+use App\Entity\User;
+use App\Entity\Drink;
+use App\Entity\Ingredient;
+use App\Entity\Cart;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use App\Controller\Admin\PizzaCrudController;
+
 
 class DashboardController extends AbstractDashboardController
 {
+    // constructeur d'url
+    public function __construct(private AdminUrlGenerator $AdminUrlGenerator){}
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        $url = $this-> AdminUrlGenerator
+        ->setController(PizzaCrudController::class)
+        ->generateUrl();
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('PizzeriaSymfony');
+            ->setTitle('Le Patron de la Pizzeria');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToRoute('Retour à l\'accueil', 'fa fa-home', 'app_accueil');
+        yield MenuItem::linkToCrud('Menu', 'fas fa-cutlery ', Menu::class);
+        yield MenuItem::linkToCrud('Pizza', 'fas fa-pie-chart ', Pizza::class);
+        yield MenuItem::linkToCrud('Ingredient', 'fas fa-slideshare', Ingredient::class);
+        yield MenuItem::linkToCrud('Users', 'fas fa-users', User::class);
+        yield MenuItem::linkToCrud('Cart', 'fas fa-cart-arrow-down', Cart::class);
+        yield MenuItem::linkToCrud('Drink', 'fas fa-beer ', Drink::class);
     }
 }
